f99bc68ad4b8be7c03d65c05a771a8ca4324db2d
Amir Hesam Gholami <140734166+Amir-Hesam-Gholami@users.noreply.github.com>
2024-08-16 14:57:01 +0330

    some change

--- src/components/Screen/screen.jsx
+++ src/components/Screen/screen.jsx
@@ -1,9 +1,8 @@
 // import { Textfit } from "react-textfit";
 import "./screen.css";
-import "../../"
 const Screen = ({ value }) => {
   return (
-    <div className="screen lalezar-regular" mode="single" max={70}>
+    <div className="screen" mode="single" max={70}>
       {value}
     </div>
   );
